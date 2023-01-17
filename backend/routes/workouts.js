ea1825eac39e45846ec750c733a04037d85ec404
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-01-17 17:02:56 +0000

    db set up

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -14,4 +14,12 @@ router.post('/', (req, res) => {
   res.json({mssg: 'POST a new workout'})
 })
 
+router.delete('/:id', (req, res) => {
+  res.json({mssg: 'DELETE a workout'})
+})
+
+router.patch('/:id', (req, res) => {
+  res.json({mssg: 'UPDATE a workout'})
+})
+
 module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,12 @@ require('dotenv').config()
 
 const express = require('express');
 const workoutRoutes = require('./routes/workouts')
+const mongoose = require('mongoose')
 
 // express app
 const app = express()
+
+app.use(express.json())
 
 // middleware for each response and request, prints out the path and the method to the console
 app.use((req, res, next) => {
@@ -14,6 +17,17 @@ app.use((req, res, next) => {
 
 // routes
 app.use('/api/workouts', workoutRoutes)
+
+mongoose.connect('mongodb://localhost:27017/mernworkout', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully');
+});
 
 // listen for requests
 app.listen(process.env.PORT, () => {
